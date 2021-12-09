6afc58581adc4c4ed46c40f1ed77f07afa47d461
skriptslo <skriptslo@outlook.com>
2021-12-09 14:21:50 -0800

    Code Cleanup

--- CarBlazor/Startup.cs
+++ CarBlazor/Startup.cs
@@ -1,19 +1,11 @@
 using Blazorise;
 using Blazorise.Bootstrap;
-using CarBlazor.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NHibernate.Mapping.ByCode;
-using NHibernate.Mapping.ByCode.Impl;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace CarBlazor
 {
